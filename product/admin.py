72e6d24e307cc55d67855ec6e6c8c784a6862f8a
Umesh Singh Rawat <rawatumesh3457@gmail.com>
2025-07-29 09:14:12 +0530

    added home page

---
+++ product/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Product
+
+# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('company_name', 'product_name', 'price', 'stock', 'category', 'modified_date', 'is_available')
+    list_display_links = ('company_name', 'product_name')
+    prepopulated_fields = {'slug' : ('product_name', 'company_name')}
+
+admin.site.register(Product, ProductAdmin)
---
+++ product/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from category.models import Category
+
+# Create your models here.
+
+class Product(models.Model):
+    company_name    = models.CharField(max_length=100, default='Vogona')
+    product_name    = models.CharField(max_length=100, unique=True)
+    slug            = models.SlugField(max_length=100, unique=True)
+    discription     = models.TextField(max_length=500, blank=True)
+    price           = models.IntegerField()
+    images          = models.ImageField(upload_to='product_images')
+    stock           = models.IntegerField()
+    is_available    = models.BooleanField(default=True)
+    category        = models.ForeignKey(Category, on_delete=models.CASCADE)
+    create_date     = models.DateTimeField(auto_now_add=True)
+    modified_date   = models.DateTimeField(auto_now_add=True)
+
+
+
+    def __str__(self):
+        return self.product_name
