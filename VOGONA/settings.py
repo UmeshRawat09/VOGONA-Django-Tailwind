67f61da555a49760d8cb6289c08cbf082981d373
Umesh Singh Rawat <rawatumesh3457@gmail.com>
2025-08-07 08:18:01 +0530

    smtp commant in settings.py

--- VOGONA/settings.py
+++ VOGONA/settings.py
@@ -153,8 +153,8 @@ MESSAGE_TAGS = {
 }
 
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'umeshsinghrawat8954@gmail.com'
-EMAIL_HOST_PASSWORD = 'hjvk nbyg rxwq xhgi'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = '##########@gmail.com'
+# EMAIL_HOST_PASSWORD = 'hjvk nbyg rxwq xhgi'
